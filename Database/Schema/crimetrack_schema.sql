0f5d264bf51d63a7c021ba96d7a1fadb7dd0ab2c
Michael Clark-Cuadrado <michael@michaelclarkcuadrado.com>
2018-02-13 11:46:45 -0500

    Finish schema

--- Database/Schema/crimetrack_schema.sql
+++ Database/Schema/crimetrack_schema.sql
@@ -1,6 +1,7 @@
-/*
- *  CrimeTrack Database. Seeded from City of Chicago Crimes 2001 to Present
- */
+--
+-- CrimeTrack Database. Seeded from City of Chicago Crimes 2001 to Present
+--
+CREATE DATABASE `crime_s18`;
 USE crime_s18;
 
 # Import dataset into temporary table to seed real tables
@@ -28,13 +29,22 @@ CREATE TABLE temp_import_dataset (
 	LONGITUDE VARCHAR(255) NOT NULL,
 	LOCATION VARCHAR(255) NOT NULL );
 
-# LOAD DATA LOCAL INFILE "Crimes_-_2001_to_present.csv"  INTO TABLE temp_import_dataset IGNORE 1;
+# chicago crimes dataset available from data.gov, not included in repo
+LOAD DATA INFILE '/docker-entrypoint.d/Crimes_-_2001_to_present.csv' INTO TABLE temp_import_dataset IGNORE 1 LINES;
 
 CREATE TABLE crimetrack_location_types (
 	location_ID INT(11) AUTO_INCREMENT NOT NULL,
 	location_desc VARCHAR(255)  NOT NULL,
 	PRIMARY KEY(location_ID)
 );
+
+# seed locations from imported dataset
+INSERT INTO crimetrack_location_types (location_desc) 
+	SELECT DISTINCT LOCATION_DESCRIPTION FROM temp_import_dataset;
+
+UPDATE temp_import_dataset 
+JOIN crimetrack_location_types 
+ON LOCATION_DESCRIPTION = location_desc SET LOCATION_DESCRIPTION = location_ID;
 
 CREATE TABLE crimetrack_crime_type (
 	IUCR_PK SMALLINT(3) NOT NULL,
@@ -43,12 +53,17 @@ CREATE TABLE crimetrack_crime_type (
 	PRIMARY KEY(IUCR_PK)
 );
 
+INSERT INTO crimetrack_crime_type (IUCR_PK, crime_type, crime_description)
+	SELECT DISTINCT IUCR, PRIMARY_TYPE, DESCRIPTION FROM temp_import_dataset;
+
 CREATE TABLE crimetrack_chicago_community_areas (
 	area_id SMALLINT(3) NOT NULL,
 	community_name VARCHAR(255),
 	population VARCHAR(255),
 	income VARCHAR(255)
 );
+
+LOAD DATA INFILE '/docker-entrypoint.d/chicago-community-areas.csv' INTO TABLE crimetrack_chicago_community_areas IGNORE 1 LINES;
 
 CREATE TABLE crimetrack_users (
 	user_id INT(11) AUTO_INCREMENT NOT NULL,
