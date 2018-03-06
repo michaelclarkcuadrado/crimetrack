00ded205be1ef7fa454c712a8e4b2718fc7604c4
Unknown <tangyi01@gettysburg.edu>
2018-03-06 17:59:31 -0500

    Fix sign-up problems, separate style in a css

--- Application/src/api/addUser.php
+++ Application/src/api/addUser.php
@@ -1,21 +1,29 @@
 <?php
 	include_once('config.php');
 
-	print_r($_POST);
-
 	$username = $_POST['username'];
 	$email = $_POST['email'];
 	$password = $_POST['password'];
 
-	$add = "INSERT INTO crimetrack_users VALUE('$username', '$email', '$password', 'common');";
-	
-	print $add;
+	$add = "INSERT INTO crimetrack_users VALUE(NULL, '$username', '$email', '$password', 'common');";
 
 	$result = $db->query($add);
 
 	if($result != FALSE) {
-		print "<p>new user named " . $username. " has been added.</p>";
+//		print "<p>new user named " . $username. " has been added.</p>";
 	}
 	else{
 	}
 ?>
+
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="../style_sign-up.css">
+
+<title>Sign up successfully</title>
+</head>
+<body>
+	<h3>You have successfully signed up.<br /><br />
+      <a href="../index.html">Click here to login</a></h2>
+</body>
+</html>
