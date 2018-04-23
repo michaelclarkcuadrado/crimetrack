ae9f4474b76c036fb0e524cc139849f8c9d13fd5
Unknown <boucbe01@gettysburg.edu>
2018-04-22 20:37:14 -0400

    interacts with database sends temp password; 1 error now

--- Application/src/sendEmail.php
+++ Application/src/sendEmail.php
@@ -1,17 +1,53 @@
 <!DOCTYPE HTML>
 <?php
+	//function found online at http://codepad.org/UL8k4aYK; works with program now
+	function randomPassword() {
+		$alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+		$pass = array(); //remember to declare $pass as an array
+		$alphaLength = strlen($alphabet) - 1; //put the length -1 in cache 
+		for ($i = 0; $i < 8; $i++) {
+			$n = rand(0, $alphaLength);
+			$pass[] = $alphabet[$n];
+		}
+		echo implode($pass);
+		return implode($pass); //turn the array into a string
+	}
+	
+	
 	//code to send email to user
 
 	$email   = $_POST['emailSend'];   
 	$name    = $_POST['usernameSend'];    
-
+	
+	//code to check if user is in database
+	session_start();
+	global $db;
+	$checkUname = "SELECT * FROM `crimetrack_users` WHERE username = '$name'";
+	$unameResult = $db->query($checkUname);
+	if($unameResult == FALSE) {
+		die("Database refused to respnse.");
+	}
+	if($unameResult->rowCount() == 0) {
+		$msg = "Username does not exists.";
+		echo "<script type='text/javascript'>alert('$msg');</script>";
+	}
+	else{
+		$getUID = "SELECT user_id FROM `crimetrack_users` WHERE username = '$name'";
+		$uid = $db->query($getUID);
+	}
 
 	$to      = $_POST['emailSend'];
-	$subject = "Comment from $name";
+	$subject = "Password for $name";
 
-	$header  = "From: $name <$email>\r\n";
-	//need a field content which will hold the users password or a system generated password
-	$result  = mail($to, $subject, $header);
+	$password = randomPassword();
+	
+	$din = "UPDATE crimetrack_users"."SET password_hash = $password WHERE user_id=$getUID";
+	$db->query($din);
+	//UPDATE crimetrack_users SET password_hash = "" WHERE user_id=
+	
+	
+	$content  = "Password for $name <$email>. $password \r \n"; //link to changePassword
+	$result  = mail($to, $subject, $content);
 ?>
 
 <HTML>
