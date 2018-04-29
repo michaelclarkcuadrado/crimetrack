331f3da97ab7f92bf2fcd5f5b1d75e4e383dff8c
Tracy Tang <tangyi01@gettysburg.edu>
2018-04-29 15:43:36 -0400

    Add downloadcsv.php to /api

---
+++ Application/src/api/downloadcsv.php
@@ -0,0 +1,47 @@
+<?php
+    include_once("config.php");
+
+    $crimeType = json_decode($_POST['crimeType']);
+    $neighborhood = json_decode($_POST['neighborhood']);
+    $month = $_POST['month'];
+    $year = $_POST['year'];
+
+    $typeString = "(";
+    foreach ($crimeType as $type) {
+        $typeString .= "'" . $type . "',";
+    }
+    $strlen = strlen($typeString);
+    $typeString = substr($typeString, 0, $strlen-1);
+    $typeString .= ")";
+    print $typeString;
+
+    $neighborhoodString = "(";
+    foreach ($neighborhood as $n) {
+        $neighborhoodString .= "'" . $n . "',";
+    }
+    $strlen = strlen($neighborhoodString);
+    $neighborhoodString = substr($neighborhoodString, 0, $strlen-1);
+    $neighborhoodString .= ")";
+    print $neighborhoodString;
+
+    $getCrime = "SELECT `DATE`, STREET, crime_type, community_name, LOCATION_DESCRIPTION, ARREST, DOMESTIC, LATITUDE, LONGITUDE
+                FROM (`crimetrack_crimes` JOIN `crimetrack_chicago_community_areas` ON COMMUNITY_AREA = area_id)
+                                          JOIN `crimetrack_crime_type` ON  IUCR = IUCR_PK 
+                WHERE IUCR IN '$typeString' AND area_id IN '$neighborhoodString'
+                      AND MONTH(`DATE`) = '$month' AND YEAR(`DATE`) = '$year'";
+    $crimeResult = $db->query($getCrime);
+
+    $filename = "Chicago_Crime_Selection_" . $month . "_" . $year;
+    header('Content-Type: application/csv');
+    header('Content-Disposition: attachment; filename="'.$filename.'";');
+    
+    // open the "output" stream
+    // see http://www.php.net/manual/en/wrappers.php.php#refsect2-wrappers.php-unknown-unknown-unknown-descriptioq
+    $f = fopen('php://output', 'w');
+    $headers = array("Date", "Street Adress", "Crime Type", "Community", "Location Type", "Arrested?", "Domestic?", "Latitude", "Longitude");
+    fputcsv($f, $headers);
+    while($row = $crimeResult->fetch(PDO::FETCH_ASSOC)){
+        fputcsv($f, $row);
+    }
+   
+?>
