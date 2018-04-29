6f470ab1fdbe700ad52ecb53fd1cdfe0c7eb63b9
Unknown <boucbe01@gettysburg.edu>
2018-04-29 16:58:10 -0400

    Fixed

--- Application/src/sendEmail.php
+++ Application/src/sendEmail.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML>
 <?php
-	include_once('config.php');
+	include_once('api/config.php');
 	//does this connect to database?
 
 	//function found online at http://codepad.org/UL8k4aYK; works with program now
@@ -12,7 +12,7 @@
 			$n = rand(0, $alphaLength);
 			$pass[] = $alphabet[$n];
 		}
-		echo implode($pass);
+		//echo implode($pass);
 		return implode($pass); //turn the array into a string
 	}
 	
@@ -25,9 +25,9 @@
 	//code to check if user is in database
 	
 	$checkUname = "SELECT * FROM `crimetrack_users` WHERE username = '$name'";
-	echo "pre query1";
-	$unameResult = $db->query($checkUname);//!!!!!!!!!!!!This is where it breaks idk why its same as Trishs!!!!!!!!!!!!!!!!!!!!
-	echo "we made it";
+	
+	$unameResult = $db->query($checkUname);//!!!!!!!!!!!!This is where it breaks idk why its same as Ts!!!!!!!!!!!!!!!!!!!!
+
 	if($unameResult == FALSE) {
 		die("Database refused to respnse.");
 	}
@@ -36,7 +36,6 @@
 		echo "<script type='text/javascript'>alert('$msg');</script>";
 	}
 	else{
-		echo "tryna get uid";
 		$getUID = "SELECT user_id FROM `crimetrack_users` WHERE username = '$name'";
 		$uid = $db->query($getUID);
 	}
@@ -46,10 +45,8 @@
 	$subject = "Password for $name";
 	$password = randomPassword();
 	
-	echo "at last query";
 	$din = "UPDATE crimetrack_users"."SET password_hash = $password WHERE user_id=$getUID";
 	$db->query($din);
-	echo "through query:)";
 	
 	
 	$content  = "Password for $name <$email>. $password. \r \n http://cs.gettysburg.edu/~boucbe01/cs360_s18/crimeTest/changePassword.html \n"; // update link to changePassword
@@ -63,7 +60,7 @@
 
 	<BODY>
 		<!--informs user that the process went through -->
-		<H2>Here is your password.</H2>
+		<H2>Here is your password.  </H2> <?php echo $password?>
 		<P>Your name: <?php echo $name;  ?></P>
 		<P>Your mail: <?php echo $email; ?></P>
 	</BODY>
